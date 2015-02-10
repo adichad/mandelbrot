25f7fc478a5637f09ffe19612d0cd1c9590d9237
Aditya Varun Chadha <adichad@gmail.com>
2015-02-11 03:10:53 +0530

    relevance tweaks

--- src/main/scala/com/askme/mandelbrot/handler/SearchRequestHandler.scala
+++ src/main/scala/com/askme/mandelbrot/handler/SearchRequestHandler.scala
@@ -288,9 +288,9 @@ class SearchRequestHandler(val config: Config, serverContext: SearchContext) ext
         fullFields.foreach {
           field: (String, Float) => {
             val k = w.mkString(" ")
-            kwquery.add(nestIfNeeded(field._1, termQuery(field._1, k).boost(field._2 * 262144f * w.length * w.length * (searchFields.size + condFields.values.size + 1))))
+            kwquery.add(nestIfNeeded(field._1, termQuery(field._1, k).boost(field._2 * 2097152f * w.length * w.length * (searchFields.size + condFields.values.size + 1))))
             val ck = kw.replaceAll("""[^a-zA-Z0-9]+""", " ").trim.toLowerCase
-            kwquery.add(nestIfNeeded(field._1, termQuery(field._1, ck).boost(field._2 * 262144f * w.length * w.length * (searchFields.size + condFields.values.size + 1))))
+            kwquery.add(nestIfNeeded(field._1, termQuery(field._1, ck).boost(field._2 * 2097152f * w.length * w.length * (searchFields.size + condFields.values.size + 1))))
           }
         }
         kwquery.add(strongMatch(searchFields, condFields, w, kw, fuzzyprefix, fuzzysim))
