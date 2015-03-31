bb8c20a148ef407121442e00af8cfdb601b9eff7
Aditya Varun Chadha <adichad@gmail.com>
2015-03-31 07:12:57 +0530

    compilation error\!?

--- src/main/scala/com/askme/mandelbrot/plugin/RecombiningKeywordTokenFilter.scala
+++ src/main/scala/com/askme/mandelbrot/plugin/RecombiningKeywordTokenFilter.scala
@@ -4,18 +4,15 @@ import org.apache.lucene.analysis.TokenStream
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.index.Index
 import org.elasticsearch.index.analysis.AbstractTokenFilterFactory
-import org.elasticsearch.index.settings.IndexSettings
-
-import scala.annotation.meta.field
 
 /**
  * Created by adichad on 30/03/15.
  */
 
 
-class RecombiningKeywordTokenFilter(override val index: Index,
-                                    @(IndexSettings @field) indexSettings: Settings,
-                                    override val name: String,
+class RecombiningKeywordTokenFilter(index: Index,
+                                    indexSettings: Settings,
+                                    name: String,
                                     settings: Settings)
   extends AbstractTokenFilterFactory(index, indexSettings, name, settings) {
   override def create(tokenStream: TokenStream): TokenStream = ???
