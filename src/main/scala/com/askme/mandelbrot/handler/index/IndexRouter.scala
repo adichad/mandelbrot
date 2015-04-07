d3376a33c386102a73c3a5eb1972bef434fe9f83
Aditya Varun Chadha <adichad@gmail.com>
2015-04-08 02:41:32 +0530

    more trials

--- src/main/scala/com/askme/mandelbrot/handler/index/IndexRouter.scala
+++ src/main/scala/com/askme/mandelbrot/handler/index/IndexRouter.scala
@@ -6,7 +6,7 @@ import com.askme.mandelbrot.handler._
 import com.askme.mandelbrot.handler.message.IndexParams
 import com.typesafe.config.Config
 import spray.http.MediaTypes._
-import spray.http.{HttpRequest, RemoteAddress, StatusCodes}
+import spray.http.{HttpCharsets, HttpRequest, RemoteAddress, StatusCodes}
 
 /**
  * Created by adichad on 31/03/15.
@@ -20,15 +20,16 @@ case class IndexRouter(val config: Config) extends Router with Configurable {
         path("index" / Segment / Segment ) { (index, esType) =>
           parameters('charset.as[String] ? "UTF8") { (charset) =>
             if (boolean("enabled")) {
-              entity(as[Array[Byte]]) { data =>
-                val d = new String(data, charset)
-                info(d)
+              extract(_.request.entity.asString(HttpCharsets.`UTF-8`)) {data =>
+              //entity(as[Array[Byte]]) { data =>
+                //val d = new String(data, charset)
+                //info(d)
                 respondWithMediaType(`application/json`) {
                   ctx => context.actorOf(Props(classOf[IndexRequestCompleter], service.config, serverContext, ctx,
                     IndexingParams(
                       RequestParams(httpReq, clip, clip.toString),
                       IndexParams(index, esType),
-                      RawData(d),
+                      RawData(data),
                       System.currentTimeMillis
                     )))
                 }
