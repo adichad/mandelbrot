e73aa419515a18aa122303d2f5d20f618ac6e2a8
Aditya Varun Chadha <adichad@gmail.com>
2015-04-18 13:02:40 +0530

    fix compile error

--- src/main/scala/com/askme/mandelbrot/util/Utils.scala
+++ src/main/scala/com/askme/mandelbrot/util/Utils.scala
@@ -7,13 +7,13 @@ import scala.util.matching.Regex
  */
 object Utils {
 
+  private val specials: Seq[(String, String)] = ("\\", "\\\\") +: (("\"", "\\\"") +: (0x0000.toChar.toString, "") +:
+    (for (c <- (0x0001 to 0x001F)) yield (c.toChar.toString, "\\u" + ("%4s" format Integer.toHexString(c)).replace(" ", "0"))))
+
   implicit class `string utils`(val s: String) extends AnyVal {
     def nonEmptyOrElse(other: => String) = if (s.isEmpty) other else s
 
     def tokenize(regex: Regex): List[List[String]] = regex.findAllIn(s).matchData.map(_.subgroups).toList
-
-    val specials: Seq[(String, String)] = ("\\", "\\\\") +: (("\"", "\\\"") +: (0x0000.toChar.toString, "") +:
-      (for (c <- (0x0001 to 0x001F)) yield (c.toChar.toString, "\\u" + ("%4s" format Integer.toHexString(c)).replace(" ", "0"))))
 
     def escapeJson: String = {
 
