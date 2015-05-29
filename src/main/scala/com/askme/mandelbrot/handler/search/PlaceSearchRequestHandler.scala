b06cac4fa0d758a7e1f55dd9fc2ee94799948c7d
adichad <adichad@gmail.com>
2015-05-29 15:38:38 +0530

    1e49 b*

--- src/main/scala/com/askme/mandelbrot/handler/search/PlaceSearchRequestHandler.scala
+++ src/main/scala/com/askme/mandelbrot/handler/search/PlaceSearchRequestHandler.scala
@@ -280,7 +280,7 @@ object PlaceSearchRequestHandler extends Logging {
 
   private val emptyStringArray = new Array[String](0)
 
-  private def superBoost(len: Int) = math.pow(10, len-1).toFloat * (searchFields.size + condFields.values.size + 1)
+  private def superBoost(len: Int) = math.pow(10, math.min(10,len-1)).toFloat * (searchFields.size + condFields.values.size + 1)
   private val paidFactor = 1e14f
 
 
