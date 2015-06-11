72247db8fd115b8ef21617a239a178a1c514f242
adichad <adichad@gmail.com>
2015-06-11 09:48:43 +0530

    simplify exact match detection.

--- src/main/scala/com/askme/mandelbrot/scripts/ExactNameMatchScript.scala
+++ src/main/scala/com/askme/mandelbrot/scripts/ExactNameMatchScript.scala
@@ -19,33 +19,10 @@ class ExactNameMatch extends NativeScriptFactory with Logging {
 
 class ExactNameMatchScript(name: String) extends AbstractLongSearchScript with Logging {
 
-
   override def runAsLong: Long = {
-    val analyzer = doc.mapperService().analysisService().analyzer("keyword_analyzer").analyzer()
-    val tokenStream = analyzer.tokenStream("LocationNameExact", doc.get("LocationNameExact").asInstanceOf[Strings].getValue)
-    tokenStream.reset()
-    val termAttribute: CharTermAttribute = tokenStream.addAttribute(classOf[CharTermAttribute])
-    tokenStream.incrementToken()
-    var ret = if(new String(termAttribute.buffer, 0, termAttribute.length) == name) 0 else 1
-    tokenStream.end()
-    tokenStream.close()
-
-    if(ret==1) {
-      val aliases = doc.get("CompanyAliasesExact").asInstanceOf[Strings].getValues
-
-      if(aliases.size<1)
-        return 1
-
-      ret = aliases.map(a=>analyzer.tokenStream("CompanyAliasesExact", a)).map { tokenStream =>
-        tokenStream.reset()
-        val termAttribute = tokenStream.addAttribute(classOf[CharTermAttribute])
-        tokenStream.incrementToken()
-        val iret = if(new String(termAttribute.buffer, 0, termAttribute.length) == name) 0 else 1
-        tokenStream.end()
-        tokenStream.close()
-        iret
-      }.min
-    }
-    ret
+    if(doc.get("LocationNameExact").asInstanceOf[Strings].getValues.filter(_ == name).size > 0 ||
+      doc.get("CompanyAliasesExact").asInstanceOf[Strings].getValues.filter(_ == name).size > 0) 0
+    else
+      1
   }
 }
