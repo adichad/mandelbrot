c1e9cec80a330292ce1d603492eca5ffd2f290c3
adichad <adichad@gmail.com>
2015-07-02 00:44:41 +0530

    log in error log for npes in transformation

--- src/main/scala/com/askme/mandelbrot/scripts/MediaCount.scala
+++ src/main/scala/com/askme/mandelbrot/scripts/MediaCount.scala
@@ -4,6 +4,7 @@ import java.lang.Cloneable
 import java.util
 
 import com.askme.mandelbrot.server.RootServer
+import grizzled.slf4j.Logging
 import org.elasticsearch.action.admin.indices.analyze.AnalyzeRequestBuilder
 import org.elasticsearch.action.search.SearchType
 import org.elasticsearch.index.query.QueryBuilders._
@@ -26,7 +27,7 @@ class MediaCount extends NativeScriptFactory {
   }
 }
 
-class MediaCountScript(private val esClient: Client, index: String, esType: String) extends AbstractExecutableScript {
+class MediaCountScript(private val esClient: Client, index: String, esType: String) extends AbstractExecutableScript with Logging {
   val vars = new util.HashMap[String, AnyRef]()
   val search = esClient.prepareExists(index).setTypes(esType)
 
@@ -47,39 +48,44 @@ class MediaCountScript(private val esClient: Client, index: String, esType: Stri
   }
 
   override def run(): AnyRef = {
-    if (vars.containsKey("ctx") && vars.get("ctx").isInstanceOf[util.Map[String,AnyRef]]) {
-      val ctx = vars.get("ctx").asInstanceOf[util.Map[String,AnyRef]]
-      if (ctx.containsKey("_source") && ctx.get("_source").isInstanceOf[util.Map[String,AnyRef]]) {
-        val source = ctx.get("_source").asInstanceOf[util.Map[String,AnyRef]]
+    try {
+      if (vars.containsKey("ctx") && vars.get("ctx").isInstanceOf[util.Map[String, AnyRef]]) {
+        val ctx = vars.get("ctx").asInstanceOf[util.Map[String, AnyRef]]
+        if (ctx.containsKey("_source") && ctx.get("_source").isInstanceOf[util.Map[String, AnyRef]]) {
+          val source = ctx.get("_source").asInstanceOf[util.Map[String, AnyRef]]
 
-        // media count
-        val mediaCount: Int = source.get("Media").asInstanceOf[util.ArrayList[AnyRef]].size +
-          (if(XContentMapValues.nodeStringValue(source.get("CompanyLogoURL"), "") == "") 0 else 1) +
-          source.get("Product").asInstanceOf[util.ArrayList[AnyRef]]
-            .map(p=>XContentMapValues.nodeStringValue(p.asInstanceOf[util.Map[String, AnyRef]].get("imageurls"), "")).filter(_!="").length
-        source.put("MediaCount", new java.lang.Integer(mediaCount))
+          // media count
+          val mediaCount: Int = source.get("Media").asInstanceOf[util.ArrayList[AnyRef]].size +
+            (if (XContentMapValues.nodeStringValue(source.get("CompanyLogoURL"), "") == "") 0 else 1) +
+            source.get("Product").asInstanceOf[util.ArrayList[AnyRef]]
+              .map(p => XContentMapValues.nodeStringValue(p.asInstanceOf[util.Map[String, AnyRef]].get("imageurls"), "")).filter(_ != "").length
+          source.put("MediaCount", new java.lang.Integer(mediaCount))
 
-        // augment noisy attribute values with pkws
-        source.get("Product").asInstanceOf[util.ArrayList[AnyRef]].foreach { p=>
-          val catkws = new util.ArrayList[AnyRef](p.asInstanceOf[util.Map[String,AnyRef]].get("categorykeywords").asInstanceOf[util.ArrayList[AnyRef]].filter(!XContentMapValues.nodeStringValue(_, "").trim.isEmpty))
-          catkws.append(XContentMapValues.nodeStringValue(p.asInstanceOf[util.Map[String,AnyRef]].get("l3category"), ""))
-          p.asInstanceOf[util.Map[String,AnyRef]].get("stringattribute").asInstanceOf[util.ArrayList[AnyRef]].foreach { a =>
-            a.asInstanceOf[util.Map[String, AnyRef]].put("answerexact", new util.ArrayList[AnyRef](a.asInstanceOf[util.Map[String, AnyRef]].get("answer").asInstanceOf[util.ArrayList[AnyRef]].map(ans=>mapAttributes(XContentMapValues.nodeStringValue(ans, ""), catkws)).flatten))
+          // augment noisy attribute values with pkws
+          source.get("Product").asInstanceOf[util.ArrayList[AnyRef]].foreach { p =>
+            val catkws = new util.ArrayList[AnyRef](p.asInstanceOf[util.Map[String, AnyRef]].get("categorykeywords").asInstanceOf[util.ArrayList[AnyRef]].filter(!XContentMapValues.nodeStringValue(_, "").trim.isEmpty))
+            catkws.append(XContentMapValues.nodeStringValue(p.asInstanceOf[util.Map[String, AnyRef]].get("l3category"), ""))
+            p.asInstanceOf[util.Map[String, AnyRef]].get("stringattribute").asInstanceOf[util.ArrayList[AnyRef]].foreach { a =>
+              a.asInstanceOf[util.Map[String, AnyRef]].put("answerexact", new util.ArrayList[AnyRef](a.asInstanceOf[util.Map[String, AnyRef]].get("answer").asInstanceOf[util.ArrayList[AnyRef]].map(ans => mapAttributes(XContentMapValues.nodeStringValue(ans, ""), catkws)).flatten))
+            }
           }
+
+          // create analyzed doc-value fields
+          source.put("LocationNameDocVal", analyze(esClient, index, "LocationNameExact", source.get("LocationName").asInstanceOf[String]).mkString(" "))
+          val aliases =
+            if (source.get("CompanyAliases") == null) new util.ArrayList[String]()
+            else new util.ArrayList[AnyRef](source.get("CompanyAlaises").asInstanceOf[util.ArrayList[AnyRef]].map(a => analyze(esClient, index, "CompanyAliasesExact", XContentMapValues.nodeStringValue(a, "")).mkString(" ")))
+          source.put("CompanyAliasesDocVal", aliases)
+
         }
-
-        // create analyzed doc-value fields
-        source.put("LocationNameDocVal", analyze(esClient, index, "LocationNameExact", source.get("LocationName").asInstanceOf[String]).mkString(" "))
-        val aliases =
-          if(source.get("CompanyAliases")==null) new util.ArrayList[String]()
-          else new util.ArrayList[AnyRef](source.get("CompanyAlaises").asInstanceOf[util.ArrayList[AnyRef]].map(a=>analyze(esClient, index, "CompanyAliasesExact", XContentMapValues.nodeStringValue(a, "")).mkString(" ")))
-        source.put("CompanyAliasesDocVal", aliases)
-
+        // return the context
+        return ctx
       }
-      // return the context
-      return ctx
+      // shouldn't ever happen
+      return null
+    } catch {
+      case e: Throwable => error(e.getMessage, e)
+        throw e
     }
-    // shouldn't ever happen
-    return null
   }
 }
