9729b6165605132c6e3ebdddff730f4a0bcc610b
adichad <adichad@gmail.com>
2015-07-29 11:30:51 +0530

    short-circuit distance calculation for efficiency while sorting

--- src/main/scala/com/askme/mandelbrot/scripts/GeoBucket.scala
+++ src/main/scala/com/askme/mandelbrot/scripts/GeoBucket.scala
@@ -33,20 +33,14 @@ object GeoBucketScript {
 class GeoBucketScript(lat: Double, lon: Double, areas: Set[String], coordfield: String, areafield: String, synfield: String, skufield: String, buckets: Array[Double]) extends AbstractLongSearchScript {
   import GeoBucketScript._
   override def runAsLong: Long = {
-    val dist = if(lat!=0||lon!=0)
-      doc.get(coordfield).asInstanceOf[ScriptDocValues.GeoPoints].distanceInKm(lat, lon)
-    else 100d
-
-    val bucket = buckets.indexWhere(dist <= _)
-    if(bucket == 0)
-      0
-    else if(doc.get(areafield).asInstanceOf[Strings].getValues.filter(areas.contains(_)).size>0)
+    if(doc.get(areafield).asInstanceOf[Strings].getValues.filter(areas.contains(_)).size>0)
       0
     else if(doc.asInstanceOf[util.Map[String, util.List[String]]].getOrDefault(synfield, empty).asInstanceOf[Strings].getValues.filter(areas.contains(_)).size>0)
       0
     else if(doc.asInstanceOf[util.Map[String, util.List[String]]].getOrDefault(skufield, empty).asInstanceOf[Strings].getValues.filter(areas.contains(_)).size>0)
       0
     else
-      bucket
+      buckets.indexWhere(
+        (if(lat!=0||lon!=0) doc.get(coordfield).asInstanceOf[ScriptDocValues.GeoPoints].distanceInKm(lat, lon) else 100d) <= _)
   }
 }
