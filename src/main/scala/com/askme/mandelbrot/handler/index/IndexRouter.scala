9205d78d176ae3d51d74a34856825f4239543f3a
adichad <adichad@gmail.com>
2015-08-09 19:26:41 +0530

    try using juniversalchardet instead of tika to guess source encoding if none is declared. seems to read all input to decide.

--- src/main/scala/com/askme/mandelbrot/handler/index/IndexRouter.scala
+++ src/main/scala/com/askme/mandelbrot/handler/index/IndexRouter.scala
@@ -9,13 +9,21 @@ import com.askme.mandelbrot.handler.message.IndexParams
 import com.typesafe.config.Config
 import spray.http.MediaTypes._
 import spray.http.{HttpRequest, RemoteAddress, StatusCodes}
-import org.apache.tika.parser.txt.CharsetDetector
+import org.mozilla.universalchardet.UniversalDetector
 
 /**
  * Created by adichad on 31/03/15.
  */
 case class IndexRouter(val config: Config) extends Router with Configurable {
 
+  private def detectCharset(bytes: Array[Byte]) = {
+    val detector = new UniversalDetector(null)
+    detector.handleData(bytes, 0, bytes.length)
+    detector.dataEnd()
+    val charset = detector.getDetectedCharset
+    detector.reset()
+    charset
+  }
   override def apply(implicit service: MandelbrotHandler) = {
     import service._
     clientIP { (clip: RemoteAddress) =>
@@ -28,11 +36,8 @@ case class IndexRouter(val config: Config) extends Router with Configurable {
                 var detected = false
                 var source_charset = charset_source
                 val data = if(source_charset == "") {
-                  val detector = new CharsetDetector()
-                  val charsetsMatched = detector.setText(rawdata).detectAll()
-                  debug("charsets: " + charsetsMatched.map(_.getName).mkString(", "))
-                  val charsetMatch = charsetsMatched(0)
-                  source_charset = if (charsetMatch == null) charset_source else charsetMatch.getName
+                  val charsetMatch = detectCharset(rawdata)
+                  source_charset = if (charsetMatch == null) charset_source else charsetMatch
                   detected = charsetMatch != null
 
                   new String(new String(rawdata, Charset.forName(source_charset)).getBytes(Charset.forName(charset_target)), Charset.forName(charset_target))
