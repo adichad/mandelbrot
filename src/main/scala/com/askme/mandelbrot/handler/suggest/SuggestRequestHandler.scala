d1ac53c773e1cb141c936ccb9bb70c28ff4d88c9
adichad <adichad@gmail.com>
2015-08-22 02:13:06 +0530

    highlighting experiments

--- src/main/scala/com/askme/mandelbrot/handler/suggest/SuggestRequestHandler.scala
+++ src/main/scala/com/askme/mandelbrot/handler/suggest/SuggestRequestHandler.scala
@@ -309,9 +309,9 @@ class SuggestRequestHandler(val config: Config, serverContext: SearchContext) ex
       .setSearchType(SearchType.fromString(searchType))
       .setFrom(offset).setSize(size)
       .setFetchSource(select.split(""","""), unselect.split(""","""))
-      .addHighlightedField("targeting.kw")
+      .addHighlightedField("targeting.kw.keyword_ngram")
       .setHighlighterForceSource(true)
-      .setHighlighterType("plain")
+      .setHighlighterType("fvh")
       .addSorts(sorters)
       .setQuery(filteredQuery(query, buildFilter(suggestParams))).setHighlighterQuery(query)
 
@@ -326,7 +326,7 @@ class SuggestRequestHandler(val config: Config, serverContext: SearchContext) ex
       ).flatten
     val order = if(orders.size==1) orders.head else Terms.Order.compound(orders)
     val masters = terms("suggestions").field("groupby").order(order).size(offset+size)
-      .subAggregation(topHits("topHit").setFetchSource(select.split(""","""), unselect.split(""",""")).setHighlighterType("plain").addHighlightedField("targeting.kw").setHighlighterQuery(query).setHighlighterOptions(options).setSize(1).setExplain(explain).setTrackScores(true).addSorts(sorters))
+      .subAggregation(topHits("topHit").setFetchSource(select.split(""","""), unselect.split(""",""")).setHighlighterType("fvh").addHighlightedField("targeting.kw.keyword_ngram").setHighlighterQuery(query).setHighlighterOptions(options).setSize(1).setExplain(explain).setTrackScores(true).addSorts(sorters))
 
     if(lat != 0.0d || lon !=0.0d) {
       masters.subAggregation(min("geo").script("geobucketsuggest").lang("native").param("lat", lat).param("lon", lon).param("areas", areas))
