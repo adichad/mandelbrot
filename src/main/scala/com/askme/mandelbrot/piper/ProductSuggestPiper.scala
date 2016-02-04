54917a1277eb6f9b4fe140d605c4c3dc85b60495
adichad <adichad@gmail.com>
2016-02-05 00:40:30 +0530

    adding product suggestions piper

---
+++ src/main/scala/com/askme/mandelbrot/piper/ProductSuggestPiper.scala
@@ -0,0 +1,147 @@
+package com.askme.mandelbrot.piper
+
+import java.security.MessageDigest
+
+import com.askme.mandelbrot.server.RootServer
+import com.typesafe.config.Config
+import grizzled.slf4j.Logging
+import org.elasticsearch.action.ActionListener
+import org.elasticsearch.action.admin.indices.analyze.{AnalyzeAction, AnalyzeRequestBuilder}
+import org.elasticsearch.action.bulk.BulkResponse
+import org.elasticsearch.client.Client
+import org.json4s.JsonAST.JValue
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+import spray.json.DefaultJsonProtocol._
+import spray.json._
+
+import scala.collection.JavaConversions._
+
+/**
+ * Created by adichad on 04/01/16.
+ */
+class ProductSuggestPiper(val config: Config) extends Piper with Logging {
+  //val producer = RootServer.defaultContext.kafkaProducer
+  private def md5(s: String) =
+    MessageDigest.getInstance("MD5").digest(s.getBytes("UTF-8")).map("%02x".format(_)).mkString
+
+  val esClient = RootServer.defaultContext.esClient
+
+  private def analyze(esClient: Client, index: String, field: String, text: String): Array[String] =
+    new AnalyzeRequestBuilder(esClient.admin.indices, AnalyzeAction.INSTANCE, index, text).setField(field).get().getTokens.map(_.getTerm).toArray
+
+  implicit class ProductJValue(doc: JValue) {
+    def categories =
+      (doc \ "categories").children.map(c =>
+        (c \ "name").asInstanceOf[JString].values.trim).filter(!_.isEmpty)
+
+    def city =
+      (doc \ "subscriptions").children.map(s =>
+        (s \ "ndd_city").asInstanceOf[JString].values.trim
+      ).map(c=>if(c.isEmpty) "all city" else c)
+
+    def label = (doc \ "name").asInstanceOf[JString].values.trim
+
+    def status = (doc \ "status").asInstanceOf[JInt].values.toInt
+
+    def base_product_id = (doc \ "base_product_id").asInstanceOf[JInt].values.toString()
+
+    def kw =
+      ((doc \ "categories").children.map(c=>(c\"name").asInstanceOf[JString].values.trim).filter(!_.isEmpty) ++
+        (doc \ "categories").children.map(c=>(c\"description").asInstanceOf[JString].values.trim).filter(!_.isEmpty)) :+
+        (doc \ "name").asInstanceOf[JString].values.trim
+
+    def orders = 0
+
+    def stores = (doc\"stores").children.map(s=>(s\"name").asInstanceOf[JString].values)
+
+    def image = (doc\"image").asInstanceOf[JString].values
+
+  }
+
+
+  override def pipe(json: JValue): Unit = {
+    val startTime = System.currentTimeMillis()
+    try {
+      val bulkRequest = RootServer.defaultContext.esClient.prepareBulk
+      for(doc <- json.children) {
+        val suggestion: JValue =
+          ("id" -> doc.base_product_id) ~
+          ("targeting" ->
+            List(
+              ("city" -> doc.city) ~
+              ("kw" -> doc.kw) ~
+              ("label" -> doc.label) ~
+              ("tag" -> (List("unified", "base_product") ++ doc.stores))
+            )
+          ) ~
+          ("payload" ->
+            List(
+              ("queries" ->
+                List(
+                  ("type" -> "base_product") ~
+                  ("id" -> doc.base_product_id)
+                )
+              ) ~
+              ("display" ->
+                ("label" -> doc.label) ~
+                ("categories" -> doc.categories) ~
+                ("type" -> "base_product") ~
+                ("stores" -> doc.stores) ~
+                ("image" -> doc.image)
+              )
+            )
+          ) ~
+          ("deleted" -> doc.status) ~
+          ("groupby" -> doc.base_product_id) ~
+          ("count" -> doc.orders)
+
+
+          bulkRequest.add(
+            esClient.prepareIndex(string("params.index"), string("params.type"), doc.base_product_id.values+"-base_product")
+              .setSource(compact(render(suggestion)))
+          )
+
+        }
+        val reqSize = bulkRequest.numberOfActions()
+        bulkRequest.execute(new ActionListener[BulkResponse] {
+          override def onResponse(response: BulkResponse): Unit = {
+            try {
+              val failures = "[" + response.getItems.filter(_.isFailed).map(x => "{\""+"id"+"\": \"" + x.getId + "\", \"error\": " + x.getFailureMessage.toJson.toString + "}").mkString(",") + "]"
+              val success = "[" + response.getItems.filter(!_.isFailed).map(x => "\"" + x.getId + "\"").mkString(",") + "]"
+              val respStr = "{\"failed\": " + failures + ", \"successful\": " + success + "}"
+              if (response.hasFailures) {
+                val timeTaken = System.currentTimeMillis - startTime
+                warn("[indexing place "+string("params.index")+"."+string("params.type")+"] [" + response.getTookInMillis + "/" + timeTaken + "] [" + reqSize + "] [" + response.buildFailureMessage() + "] [" + respStr + "]")
+              }
+              else {
+                val timeTaken = System.currentTimeMillis - startTime
+                info("[indexed place "+string("params.index")+"."+string("params.type")+"] [" + response.getTookInMillis + "/" + timeTaken + "] [" + reqSize + "] [" + respStr + "]")
+              }
+            } catch {
+              case e: Throwable =>
+                val timeTaken = System.currentTimeMillis - startTime
+                error("[indexing place "+string("params.index")+"."+string("params.type")+"] [" + timeTaken + "] [" + reqSize + "] [" + e.getMessage + "]", e)
+                throw e
+            }
+          }
+
+          override def onFailure(e: Throwable): Unit = {
+            val timeTaken = System.currentTimeMillis - startTime
+            error("[indexing place "+string("params.index")+"."+string("params.type")+"] [" + timeTaken + "] [" + reqSize + "] [" + e.getMessage + "]", e)
+            throw e
+          }
+        })
+
+      } catch {
+        case e: Throwable =>
+          error("[indexing place "+string("params.index")+"."+string("params.type")+"] [" + e.getMessage + "]", e)
+          throw e
+      }
+
+  }
+
+
+
+}
