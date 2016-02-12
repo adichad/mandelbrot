8589ca95df25f3b0d09b4db9a73a4fc1481a22c6
adichad <adichad@gmail.com>
2016-02-13 03:53:10 +0530

    is_ndd changed to int

--- src/main/scala/com/askme/mandelbrot/handler/search/bazaar/ProductSearchRequestHandler.scala
+++ src/main/scala/com/askme/mandelbrot/handler/search/bazaar/ProductSearchRequestHandler.scala
@@ -19,11 +19,8 @@ import org.elasticsearch.common.ParseFieldMatcher
 import org.elasticsearch.common.unit.{Fuzziness, TimeValue}
 import org.elasticsearch.index.query.QueryBuilders._
 import org.elasticsearch.index.query._
-import org.elasticsearch.script.Script
-import org.elasticsearch.script.ScriptService.ScriptType
 import org.elasticsearch.search.aggregations.AbstractAggregationBuilder
 import org.elasticsearch.search.aggregations.AggregationBuilders._
-import org.elasticsearch.search.aggregations.bucket.terms.Terms
 import org.elasticsearch.search.aggregations.metrics.tophits.TopHitsBuilder
 import org.elasticsearch.search.sort.SortBuilders._
 import org.elasticsearch.search.sort._
@@ -64,7 +61,7 @@ object ProductSearchRequestHandler extends Logging {
     }
     else {
       (
-        Some(scriptSort(new Script("doc['subscriptions.is_ndd'].value?1:0", ScriptType.INLINE, null, null), "number").order(SortOrder.DESC).setNestedPath("subscriptions")) ::
+        Some(fieldSort("subscriptions.is_ndd").order(SortOrder.DESC).sortMode("max")) ::
           (if (store_front_id > 0)
             Some(fieldSort("subscriptions.store_fronts.boost").order(SortOrder.DESC)
               .setNestedFilter(termQuery("subscriptions.store_fronts.id", store_front_id))) else None) ::
