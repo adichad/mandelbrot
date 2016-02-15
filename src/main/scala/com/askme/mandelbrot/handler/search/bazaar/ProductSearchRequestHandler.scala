c14ed9e307e70e27832d058fdb223839f0aa7194
adichad <adichad@gmail.com>
2016-02-16 01:59:13 +0530

    store_fronts filter fix trial#7

--- src/main/scala/com/askme/mandelbrot/handler/search/bazaar/ProductSearchRequestHandler.scala
+++ src/main/scala/com/askme/mandelbrot/handler/search/bazaar/ProductSearchRequestHandler.scala
@@ -341,7 +341,7 @@ class ProductSearchRequestHandler(val config: Config, serverContext: SearchConte
     }
 
     if(store_front_id > 0) {
-      subscriptionFilter.must(
+      finalFilter.must(
         nestedQuery(
           "subscriptions.store_fronts",
           termQuery("subscriptions.store_fronts.id", store_front_id)
