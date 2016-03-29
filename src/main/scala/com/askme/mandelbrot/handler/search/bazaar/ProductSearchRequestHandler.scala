00ca7b8498640c2bd6de18efa0952bcc0cd55601
adichad <adichad@gmail.com>
2016-03-29 23:08:36 +0530

    throw 'empty search criteria' on banned phrases (fuzzy matching in later)

--- src/main/scala/com/askme/mandelbrot/handler/search/bazaar/ProductSearchRequestHandler.scala
+++ src/main/scala/com/askme/mandelbrot/handler/search/bazaar/ProductSearchRequestHandler.scala
@@ -419,6 +419,9 @@ class ProductSearchRequestHandler(val config: Config, serverContext: SearchConte
     search
   }
 
+  def bannedPhrase(w: Array[String], index: String): Boolean = {
+    list[String]("banned-phrases").contains(w.mkString(" "))
+  }
 
   override def receive = {
       case searchParams: ProductSearchParams =>
@@ -435,9 +438,9 @@ class ProductSearchRequestHandler(val config: Config, serverContext: SearchConte
           w = analyze(esClient, index, "name", kw)
           if (w.length>20) w = emptyStringArray
           w = w.take(8)
-          if (w.isEmpty && category.trim == "" &&
+          if (bannedPhrase(w, index) || (w.isEmpty && category.trim == "" &&
             product_id == 0 && grouped_id == 0 && base_id == 0 && subscribed_id == 0 && store_front_id==0 &&
-            crm_seller_id ==0 && city.trim=="") {
+            crm_seller_id ==0 && city.trim=="")) {
             context.parent ! EmptyResponse("empty search criteria")
           }
           else {
