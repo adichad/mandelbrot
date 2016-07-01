51dd04c33d69c27aa1b471cb2ab510a894be43a7
adichad <adichad@gmail.com>
2016-07-02 00:29:59 +0530

    monitor node stats on each batch indexing request

--- src/main/scala/com/askme/mandelbrot/handler/index/IndexRequestCompleter.scala
+++ src/main/scala/com/askme/mandelbrot/handler/index/IndexRequestCompleter.scala
@@ -7,6 +7,12 @@ import com.askme.mandelbrot.handler.{IndexFailureResult, IndexSuccessResult, Ind
 import com.askme.mandelbrot.server.RootServer.SearchContext
 import com.typesafe.config.Config
 import grizzled.slf4j.Logging
+import org.elasticsearch.action.ActionListener
+import org.elasticsearch.action.admin.cluster.node.stats.NodesStatsResponse
+import org.elasticsearch.action.admin.cluster.node.stats.TransportNodesStatsAction.NodeStatsRequest
+import org.elasticsearch.action.admin.indices.stats.CommonStatsFlags
+import org.elasticsearch.common.unit.TimeValue
+import org.elasticsearch.script.Template
 import org.json4s._
 import spray.http.StatusCode
 import spray.http.StatusCodes._
@@ -22,12 +28,43 @@ import spray.routing.RequestContext
 class IndexRequestCompleter(val config: Config, serverContext: SearchContext, requestContext: RequestContext, indexParams: IndexingParams) extends Actor with Configurable with Json4sSupport with Logging {
   val json4sFormats = DefaultFormats
   if(indexParams.req.trueClient.startsWith("42.120.")) {
-    warn("[" + indexParams.req.clip.toString + "]->[" + indexParams.req.httpReq.uri + "] [alibaba sala]")
+    warn("[" + indexParams.req.clip.toString + "]->[" + indexParams.req.httpReq.uri + "] [invalid request source]")
     complete(BadRequest, "invalid request source: " + indexParams.req.trueClient)
   }
   else {
-    val target = context.actorOf(Props(classOf[IndexRequestHandler], config, serverContext))
-    target ! indexParams
+    try {
+      serverContext.esClient.admin().cluster().prepareNodesStats().all().setTimeout(TimeValue.timeValueSeconds(2))
+        .execute(new ActionListener[NodesStatsResponse] {
+          override def onFailure(e: Throwable): Unit = {
+            warn(s"exception occurred while getting node stats: ${e}")
+            complete(NotAcceptable, "cluster state not conducive to indexing")
+          }
+
+          override def onResponse(response: NodesStatsResponse): Unit = {
+            val dataNodes = response.getNodes.filter(_.getNode.dataNode())
+            if (
+              dataNodes.forall(d =>
+                d.getIndices.getSegments.getIndexWriterMemory.mb < 500l
+                  && d.getIndices.getMerge.getCurrentSize.mb < 500l
+                  && d.getIndices.getSearch.getOpenContexts < 20l
+              )
+            ) {
+              val target = context.actorOf(Props(classOf[IndexRequestHandler], config, serverContext))
+              target ! indexParams
+            } else {
+              warn("cluster state not conducive to indexing: "+response.toString)
+              complete(NotAcceptable, "cluster state not conducive to indexing")
+            }
+          }
+        })
+
+    } catch {
+      case e: Exception=>
+        warn(s"exception occurred while getting node stats: ${e}")
+        complete(NotAcceptable, s"exception occurred while getting node stats: ${e}")
+    }
+
+
   }
 
   override def receive = {
