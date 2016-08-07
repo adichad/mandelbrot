9e9f7397f60ff9852afe7fa21e2a8c5189cda18a
adichad <adichad@gmail.com>
2016-08-07 11:57:08 +0530

    add dummy lat-longs everywhere

--- src/main/scala/com/askme/mandelbrot/handler/search/DealSearchRequestHandler.scala
+++ src/main/scala/com/askme/mandelbrot/handler/search/DealSearchRequestHandler.scala
@@ -597,7 +597,7 @@ class DealSearchRequestHandler(val config: Config, serverContext: SearchContext)
         val timeTaken = endTime - startTime
         val parsedResult = parse(result.toString)
         info("[" + result.getTookInMillis + "/" + timeTaken + (if (result.isTimedOut) " timeout" else "") + "] [q" + relaxLevel + "] [" + result.getHits.hits.length + "/" + result.getHits.getTotalHits + (if (result.isTerminatedEarly) " termearly (" + Math.min(maxdocspershard, int("max-docs-per-shard")) + ")" else "") + "] [" + clip.toString + "]->[" + httpReq.uri + "]")
-        context.parent ! SearchResult(result.getHits.hits.length, timeTaken, relaxLevel, parsedResult)
+        context.parent ! SearchResult(result.getHits.hits.length, timeTaken, relaxLevel, parsedResult, 0d, 0d)
       } catch {
         case e: Throwable =>
           val timeTaken = System.currentTimeMillis - startTime
